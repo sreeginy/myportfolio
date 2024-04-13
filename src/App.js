9734265cb3de3ecf93dfccabc1ba91bc24c8e726
Sreeginy <sreeginy98@gmail.com>
2024-04-13 21:46:24 +0530

    create Contact component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import About from './components/about/About'
 import Skills from './components/skills/Skills';
 import Services from './components/services/Services'
 import Qualification from './components/qualification/Qualification';
+import Testimonials from './components/testimonials/Testimonials';
+import Contact from './components/contact/Contact';
 
 function App() {
   return (
@@ -20,6 +22,8 @@ function App() {
     <Skills />
     <Services />
     <Qualification />
+    {/* <Testimonials /> */}
+    <Contact />
 
     </main>
 

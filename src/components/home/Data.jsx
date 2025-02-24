d9080bb6f508392692d4fa650e0f4977a2c002fc
Sreeginy Kodeeswaran <sreeginy98@gmail.com>
2025-02-24 23:30:27 +0530

    Update Data.jsx

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -3,7 +3,7 @@ import './Home.css';
 
 const Data = () => {
   const [jobTitle, setJobTitle] = useState("");
-  const titles = ["Android Developer", "UI/UX Designer"];
+  const titles = ["UI/UX Designer", "UI/UX Designer"];
   const [titleIndex, setTitleIndex] = useState(0);
   const [isTyping, setIsTyping] = useState(true);
 
